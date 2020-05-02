709ddbdc0a899aaa885ff62a357e8e79014f9d33
Christian Miyoshi <christianmiyoshi@hotmail.com>
2020-05-02 19:52:13 -0300

    Add minimum funcionality

---
+++ src/components/FileButton.vue
@@ -0,0 +1,3 @@
+<template>
+  <v-file-input accept=".txt" label="Select File..."></v-file-input>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,80 +1,45 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3">
+          Add Icons
+        </h2>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
+        <div class="my-2">
+          <v-btn color="primary" @click="$refs.file.click()">Upload</v-btn>
+          <input
+            type="file"
+            ref="file"
+            id="file-upload"
+            style="display:none"
+            @change="onFileChange"
+            multiple
+          />
+        </div>
       </v-col>
 
       <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">
-          What's next?
+          List Icons
         </h2>
 
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+          <v-col v-for="icon in icons" v-bind:key="icon.id" :cols="2">
+            <v-card class="mx-auto" max-width="400">
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="icon.url"
+                contain
+                aspect-ratio="1"
+              >
+              </v-img>
+              <v-card-subtitle>
+                {{ icon.filename }}
+              </v-card-subtitle>
+            </v-card></v-col
           >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
         </v-row>
       </v-col>
     </v-row>
@@ -84,60 +49,32 @@
 <script lang="ts">
 import Vue from "vue";
 
+interface IconDataIntarface {
+  id: number;
+  filename: string;
+  size: number;
+  url: string;
+}
+
 export default Vue.extend({
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+    icons: [] as IconDataIntarface[]
+  }),
+  methods: {
+    onFileChange: function(event: Event) {
+      const target = event.target as HTMLInputElement;
+      const files: IconDataIntarface[] = Array.from(target.files || []).map(
+        file => ({
+          id: Math.random(),
+          filename: file.name,
+          size: file.size,
+          url: URL.createObjectURL(file)
+        })
+      );
+      this.icons.push(...files);
+    }
+  }
 });
 </script>
